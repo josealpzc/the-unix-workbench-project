0044ca5f5306834081e3df786634d72419aa869e
steelback25 <j.antonio_lpz@hotmail.com>
2021-02-24 13:45:04 -0600

    guessing game was modified and its currently working

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,9 +1,21 @@
-o#!/usr/bin/env bash
+#!/usr/bin/env bash
 # File: guessinggame.sh
 
 function guessinggame {
  echo "Guess the number of files in the current directory"
  read response
- number_of_file=$
+ number_of_files=$( echo pwd | ls | wc -l )
 
+ while [[ $response -ne $number_of_files ]]
+ do
+  if [[ $response -lt $number_of_files ]]
+  then
+   echo "Too low"
+  else
+   echo "Too high"
+  fi
+  echo "Try again:"
+  read response
+ done
 }
+guessinggame
